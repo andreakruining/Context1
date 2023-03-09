833a40bca960835e5f7e222817ac6e00a85f4105
andreakruining <afk05@live.nl>
2023-03-09 13:54:31 +0100

    adding

--- context1 project/Assets/openApplication.cs
+++ context1 project/Assets/openApplication.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Diagnostics;
+using TMPro;
 
 public class openApplication : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI outputText;
+    [SerializeField] private Button startButton;
+
+    private void Start()
     {
-        
+        startButton.onClick.AddListener(CreateProcess);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CreateProcess()
     {
-        
+        var processInfo = new ProcessStartInfo
+        {
+            WorkingDirectory = Application.persistentDataPath,
+            WindowStyle = ProcessWindowStyle.Normal,
+            FileName = "explorer.exe",
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            RedirectStandardOutput = true,
+        };
+
+        var process = Process.Start(processInfo);
+        var result = process.StandardOutput.ReadToEnd();
+
+        process.WaitForExit();
+
+        outputText.text = result;
     }
 }
